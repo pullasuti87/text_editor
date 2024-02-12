006486e0b8a86ece08587673bc064d564930d18b
jpmakinen <pullasuti87@gmail.com>
2024-02-12 20:36:11 +0200

    setraw seems to disable flags

--- raw.py
+++ raw.py
@@ -1,7 +1,17 @@
 # raw mode -> key press is immediately sent to the program with out press Enter
+
+# notes:
+# ICANON -> reading line by line, when disabled input is read byte by byte
+# ECHO -> prints key (terminal) that is typed
+# tty.setraw seems to disable lot of flags (not sure which ones),
+# but hey it works!
+
+
 import sys
 import termios
 import tty
+
+# import signal
 
 
 def enable_raw_mode():
@@ -11,8 +21,13 @@ def enable_raw_mode():
     # save original settings
     original_termios = termios.tcgetattr(fd)
 
-    # terminal to raw mode (disables ICANON and ECHO)
+    # terminal to raw mode (disables ICANON and ECHO flags)
     tty.setraw(fd)
+
+    # seems that is not needed
+    # block (ctrl-c -> SIGINT) and (ctrl-z -> SIGTSTP)
+    # just in case, seems to be disabled by default in raw
+    # signal.pthread_sigmask(signal.SIG_BLOCK, {signal.SIGINT, signal.SIGTSTP})
 
     return original_termios
 
@@ -20,6 +35,9 @@ def enable_raw_mode():
 def disable_raw_mode(original_termios):
     # standard input
     fd = sys.stdin.fileno()
+
+    # unblock (ctrl-c -> SIGINT) and (ctrl-z -> SIGTSTP)
+    # signal.pthread_sigmask(signal.SIG_UNBLOCK, {signal.SIGINT, signal.SIGTSTP})
 
     # restore terminal settings
     termios.tcsetattr(fd, termios.TCSAFLUSH, original_termios)
@@ -30,13 +48,13 @@ def main():
     original_termios = enable_raw_mode()
 
     try:
+
         while True:
             # read one character
             c = sys.stdin.read(1)
 
             # ascii value
-            if c.isprintable():
-                print(ord(c))
+            print(ord(c), end="\r\n")
 
             # quit
             if c == "q":
@@ -44,6 +62,8 @@ def main():
     finally:
         # exit raw mode
         disable_raw_mode(original_termios)
+
+    # timeout
 
 
 if __name__ == "__main__":
