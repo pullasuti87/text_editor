0852db8d4b7c2cb0f1714c25b3a13c122cb8cab0
pullasuti87 <pullasuti87@gmail.com>
2025-01-03 23:53:50 +0200

    small_updates_how_keys_works

--- main.py
+++ main.py
@@ -26,12 +26,13 @@ END_KEY = 1006
 PAGE_UP = 1007
 PAGE_DOWN = 1008
 ENTER_KEY = 1009
+INIT = False
 
 """ terminal """
 
 
-def error_msg(msg):
-    sys.stderr.write("error-> " + msg + "\r\n")
+def error_message(m):
+    sys.stderr.write("error-> " + m + "\r\n")
     sys.exit(1)
 
 
@@ -43,7 +44,7 @@ def enable_raw_mode():
         # save original settings
         original_termios = termios.tcgetattr(fd)
     except termios.error as e:
-        error_msg(str(e))
+        error_message(str(e))
 
     # terminal to raw mode (disables ICANON and ECHO flags)
     tty.setraw(fd)
@@ -130,25 +131,26 @@ def refresh_screen():
 
 
 def draw_rows():
+    global INIT
     columns, rows = get_window_size()
 
     for i in range(rows - 2):
         sys.stdout.write("~\r\n")
-        if i == rows // 2.5:
+        if i == rows // 2.5 and INIT == False:
             title = "SerpenScripter -- version " + VERSION + "\r\n"
             centered_title = center_text(title, columns)
             sys.stdout.write("~" + centered_title)
 
 
-def center_text(msg, width):
-    padding = max((width - len(msg)) // 2, 0)
-    return " " * padding + msg
+def center_text(message, width):
+    padding = max((width - len(message)) // 2, 0)
+    return " " * padding + message
 
 
 def clear_title():
     columns, rows = get_window_size()
     column = 2
-    row = rows // 2.5
+    row = (rows // 2.5) + 2
     sys.stdout.write(f"\x1b[{int(row)};{column}H")
     # erase from cursor to end of line
     sys.stdout.write("\x1b[K")
@@ -209,7 +211,7 @@ def move_cursor_to(x, y):
     sys.stdout.flush()
 
 
-def move_multiple_lines(direction, lines):
+def move_lines(direction, lines):
     for _ in range(lines):
         move_cursor(direction)
 
@@ -221,9 +223,9 @@ def process_keypress(raw_mode):
         move_cursor(c)
 
         if c == PAGE_UP:
-            move_multiple_lines(ARROW_UP, get_window_size()[1] - 1)
+            move_lines(ARROW_UP, get_window_size()[1] - 1)
         elif c == PAGE_DOWN:
-            move_multiple_lines(ARROW_DOWN, get_window_size()[1] - 2)
+            move_lines(ARROW_DOWN, get_window_size()[1] - 2)
         elif c == HOME_KEY:
             CX = 1
             move_cursor_to(CX, CY)
@@ -233,8 +235,11 @@ def process_keypress(raw_mode):
         elif c == DEL_KEY:
             print("delete")
         elif c == ENTER_KEY:
+            if get_window_size()[1] - 3 > CY:
+                CY += 1
+                move_cursor_to(CX, CY)
             # TODO: need scroll variable to track
-            print("broken")
+            # signal.signal(signal.SIGWINCH, resize)
 
         if not isinstance(c, int) and len(c) == 1:
             if ord(c) == 17:
@@ -254,6 +259,7 @@ def process_keypress(raw_mode):
 
 
 def init_editor():
+    global INIT
     # clear screen
     refresh_screen()
 
@@ -261,6 +267,7 @@ def init_editor():
     sys.stdout.write("\x1b[?25l")
 
     draw_rows()
+    INIT = True
     if len(sys.argv) > 1:
         draw_statusline(sys.argv[1])
     else:
@@ -276,7 +283,7 @@ def init_editor():
 
 
 def main():
-    # raw mode and save settings
+
     original_termios = enable_raw_mode()
 
     # capture terminal screen size signal
